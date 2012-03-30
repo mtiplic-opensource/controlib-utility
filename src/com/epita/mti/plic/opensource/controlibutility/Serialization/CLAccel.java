2d29eb0c80da329a3ad10944930106767ce9b7a2
Benoit Vasseur <benoit.vasr@gmail.com>
2012-03-30 12:23:51 +0200

    adds pressure and accel + edit Abstract class type attribute

---
+++ src/com/epita/mti/plic/opensource/controlibutility/Serialization/CLAccel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epita.mti.plic.opensource.controlibutility.Serialization;
+
+/**
+ *
+ * @author Benoit "KIDdAe" Vasseur
+ */
+public class CLAccel extends CLSerializable {
+
+  protected Integer x;
+  protected Integer y;
+  protected Integer z;
+
+  public CLAccel()
+  {
+    this.type = "accel";
+  }
+
+  public Integer getX() {
+    return x;
+  }
+
+  public void setX(Integer x) {
+    this.x = x;
+  }
+
+  public Integer getY() {
+    return y;
+  }
+
+  public void setY(Integer y) {
+    this.y = y;
+  }
+
+  public Integer getZ() {
+    return z;
+  }
+
+  public void setZ(Integer z) {
+    this.z = z;
+  }
+}
